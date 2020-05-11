ce35943a2ee44e90c48ffbe8eba66af44c929a64
Chris Sayen <chrissayen@iMac.local>
2020-05-11 17:10:56 -0400

    typo

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var app = require('express')();
-var cors = require('cors')
 var server = require('http').createServer(app);
 var io = require('socket.io')(server,{pingTimeout: 0, pingInterval: 500, origins: '*:*'});
-
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
